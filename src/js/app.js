c0b0a82e387774c9afc04675d263c317bec7c4c8
Sam DePoule <samdepoule@gmail.com>
2023-03-12 19:36:46 -0700

    Clicking works

--- src/js/app.js
+++ src/js/app.js
@@ -29,12 +29,12 @@ var app = new Framework7({
 	// App routes
 	routes: routes,
 
-	autoDarkMode: true,
-	// darkModeChange: true,
+	// autoDarkMode: true,
+	// // darkModeChange: true,
 
-	view: {
-		browserHistory: true,
-		browserHistorySeparator: ""
-	},
+	// view: {
+	// 	browserHistory: true,
+	// 	browserHistorySeparator: ""
+	// },
 
 });
