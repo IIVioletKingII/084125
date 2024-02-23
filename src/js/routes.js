5707a63dc9044c3a885d884c9f0976f1d8748b93
Sam DePoule <samdepoule@gmail.com>
2024-02-23 11:30:47 -0800

    Route test

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,48 +1,21 @@
 
 
 
-import HomePage from '../pages/home.f7';
+// import HomePage from '../pages/home.f7';
 import HeroPage from '../pages/newHero.f7';
 // import AboutPage from '../pages/about.f7';
-import PicturesPage from '../pages/pictures.f7';
-import ResumePage from '../pages/resume.f7';
-import PictureEditor from '../pages/picture-editor.f7';
+// import PicturesPage from '../pages/pictures.f7';
+// import ResumePage from '../pages/resume.f7';
+import PictureEditor from '../pages/pictureEditor.f7';
 
 import SettingsPage from '../pages/settings.f7';
 
 import NotFoundPage from '../pages/404.f7';
 
-var routes = [
+let routes = [
 	{
 		path: '/',
-		component: HeroPage,//HomePage,
-		// tabs: [
-		// 	{
-		// 		path: '',
-		// 		id: 'hero',
-		// 		component: HeroPage
-		// 	},
-		// 	{
-		// 		path: 'about/',
-		// 		id: 'about',
-		// 		component: AboutPage
-		// 	},
-		// 	{
-		// 		path: 'pictures/',
-		// 		id: 'pictures',
-		// 		component: PicturesPage,
-		// 		routes: [
-		// 			{
-		// 				path: '/:pictureId/'
-		// 			}
-		// 		]
-		// 	},
-		// 	{
-		// 		path: 'resume/',
-		// 		id: 'resume',
-		// 		component: ResumePage
-		// 	},
-		// ],
+		component: HeroPage, // HomePage,
 	},
 	{
 		path: '/picture-editor/',
