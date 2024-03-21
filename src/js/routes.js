135d1f21304b91e8aa1b2e1daf54969adff77dea
Sam DePoule <samdepoule@gmail.com>
2024-03-21 13:29:28 -0700

    Added an in-progress matrix multiplier with cool interactabale sliders

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,6 +3,7 @@ import HeroPage from '../pages/hero.f7';
 import ConnectFourGame from '../pages/connectFour.f7';
 import PictureEditor from '../pages/pictureEditor.f7';
 import BinaryQuizPage from '../pages/binaryQuiz.f7';
+import MatrixMathPage from '../pages/matrixMath.f7';
 
 import NotFoundPage from '../pages/404.f7';
 
@@ -22,6 +23,10 @@ const routes = [
 	{
 		path: '/binary-quiz/',
 		component: BinaryQuizPage
+	},
+	{
+		path: '/matrix-math/',
+		component: MatrixMathPage
 	},
 	{
 		path: '(.*)',
